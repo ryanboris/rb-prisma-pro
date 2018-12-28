fdcc3c598a9318f3344954120d5944b278239f1d
ryanboris <ryan.boris@gmail.com>
2018-12-28 18:26:16 -0500

    prod updates

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from './resolvers/index'
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: 'ds3jlklsjdfep9ef9jfl3k3jl4jf',
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements
 })
 
