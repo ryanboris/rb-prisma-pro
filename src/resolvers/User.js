62ddfac8106ce0825c14babebc0efc6c6b266fdb
ryanboris <ryan.boris@gmail.com>
2018-12-28 17:27:06 -0500

    updates

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -3,7 +3,6 @@ const User = {
     email: {
         fragment: 'fragment userId on User { id }',
         resolve(parent, args, { request }, info) {
-            console.log(parent)
             const userId = getUserId(request, false)
             if (userId && userId === parent.id) {
                 return parent.email
