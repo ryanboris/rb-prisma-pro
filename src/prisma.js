5ff982ab58e030e8da1ef460642d5f7a509fb7c7
ryanboris <ryan.boris@gmail.com>
2018-12-28 10:02:06 -0500

    adds fragment

--- src/prisma.js
+++ src/prisma.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 import { Prisma } from 'prisma-binding'
+import { fragmentReplacements } from './resolvers/index'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466',
-    secret: process.env.PRISMA_SECRET
+    secret: process.env.PRISMA_SECRET,
+    fragmentReplacements
 })
 
 export { prisma as default }
--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,12 +1,15 @@
 import getUserId from '../utils/getUserId'
 const User = {
-    email(parent, args, { request }, info) {
-        console.log(parent)
-        const userId = getUserId(request, false)
-        if (userId && userId === parent.id) {
-            return parent.email
-        } else {
-            return null
+    email: {
+        fragment: 'fragment userId on User { id }',
+        resolve(parent, args, { request }, info) {
+            console.log(parent)
+            const userId = getUserId(request, false)
+            if (userId && userId === parent.id) {
+                return parent.email
+            } else {
+                return null
+            }
         }
     }
 }
