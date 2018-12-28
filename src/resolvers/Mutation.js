dfdfb8f2f350033c5373e1e484e2b8941a99be31
ryanboris <ryan.boris@gmail.com>
2018-12-28 12:04:25 -0500

    adds the ability to update a users password

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,19 +2,16 @@ require('dotenv').config()
 import bcrypt from 'bcryptjs'
 import getUserId from '../utils/getUserId'
 import generateToken from '../utils/generateToken'
+import hashPass from '../utils/hashPass'
+import { duplicateArgMessage } from 'graphql/validation/rules/UniqueArgumentNames'
 
 const Mutation = {
     async createUser(parent, args, { prisma }, info) {
-        const { data } = args
-        if (args.data.password.length < 8) {
-            throw new Error('Password must be 8 characters or longer.')
-        }
-
-        const password = await bcrypt.hash(data.password, 10)
+        const password = await hashPass(args.data.password)
 
         const user = await prisma.mutation.createUser({
             data: {
-                ...data,
+                ...args.data,
                 password
             }
         })
@@ -60,6 +57,10 @@ const Mutation = {
 
     async updateUser(parent, args, { prisma, request }, info) {
         const userId = getUserId(request)
+
+        if (typeof args.data.password === 'string') {
+            args.data.password = await hashPass(args.data.password)
+        }
         return prisma.mutation.updateUser(
             {
                 where: {
