301f52c496dfb2823a16b7c243a047451b1cfabc
ryanboris <ryan.boris@gmail.com>
2018-12-28 19:03:21 -0500

    begins testing

---
+++ tests/user.test.js
@@ -0,0 +1,7 @@
+test('This is my first test case.', () => {
+    expect(typeof 'should do this').toBe('string')
+})
+
+test('This is my second test case', () => {
+    expect('x').not.toBeNull()
+})
