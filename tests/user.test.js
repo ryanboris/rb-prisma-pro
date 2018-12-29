ea89dcf3bbb1ffae6e55da29273ff40e1953c6e9
ryanboris <ryan.boris@gmail.com>
2018-12-28 19:21:36 -0500

    adds more tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,7 +1,26 @@
-test('This is my first test case.', () => {
-    expect(typeof 'should do this').toBe('string')
+import { getFirstName, isValidPassword } from '../src/utils/user'
+
+test('Should return first name when given full name', () => {
+    const firstName = getFirstName('Ryan Boris')
+    expect(firstName).toBe('Ryan')
 })
 
-test('This is my second test case', () => {
-    expect('x').not.toBeNull()
+test('should return first name when given first name', () => {
+    const firstName = getFirstName('Jen')
+    expect(firstName).toBe('Jen')
+})
+
+test('should reject password shorter than 8 characters', () => {
+    const password = isValidPassword('dog')
+    expect(password).toBeFalsy()
+})
+
+test('should validate a password that is valid', () => {
+    const password = isValidPassword('doggy12234')
+    expect(password).toBeTruthy()
+})
+
+test('should reject the use of the string "password" as the password', () => {
+    const password = isValidPassword('password')
+    expect(password).toBeFalsy()
 })
