8b893ee237bf095ea9da4b0118a24ad5532b929f
ryanboris <ryan.boris@gmail.com>
2018-12-30 06:13:44 -0500

    adds more tests for posts

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,42 +1,112 @@
 import 'cross-fetch/polyfill'
 import { gql } from 'apollo-boost'
-import seedDatabase, { userOne } from './utils/seedDatabase'
+import seedDatabase, { userOne, postOne, postTwo } from './utils/seedDatabase'
 import getClient from './utils/getClient'
+import prisma from '../src/prisma'
 
 const client = getClient()
 
 beforeEach(seedDatabase)
 
 test('Should expose published posts', async () => {
-    const getPosts = gql`
-        query {
-            posts {
-                id
-                title
-                body
-                published
-            }
-        }
-    `
-    const response = await client.query({ query: getPosts })
+  const getPosts = gql`
+    query {
+      posts {
+        id
+        title
+        body
+        published
+      }
+    }
+  `
+  const response = await client.query({ query: getPosts })
 
-    expect(response.data.posts.length).toBe(1)
-    expect(response.data.posts[0].published).toBe(true)
+  expect(response.data.posts.length).toBe(1)
+  expect(response.data.posts[0].published).toBe(true)
 })
 
 test('Should fetch users posts', async () => {
-    const client = getClient(userOne.jwt)
-    const myPosts = gql`
-        query {
-            myPosts {
-                id
-                title
-                body
-                published
-            }
-        }
-    `
-    const { data } = await client.query({ query: myPosts })
+  const client = getClient(userOne.jwt)
+  const myPosts = gql`
+    query {
+      myPosts {
+        id
+        title
+        body
+        published
+      }
+    }
+  `
+  const { data } = await client.query({ query: myPosts })
 
-    expect(data.myPosts.length).toBe(2)
+  expect(data.myPosts.length).toBe(2)
+})
+
+test('should be able to update own post', async () => {
+  const client = getClient(userOne.jwt)
+  const updatePost = gql`
+    mutation {
+        updatePost(
+          id: "${postOne.post.id}",
+          data: {
+              published: false
+          }
+        ){
+            id 
+            title
+            body
+            published
+        }
+    }
+  `
+
+  const { data } = await client.mutate({ mutation: updatePost })
+  const exists = await prisma.exists.Post({
+    id: postOne.post.id,
+    published: false
+  })
+  expect(data.updatePost.published).toBe(false)
+  expect(exists).toBe(true)
+})
+
+test('should be able to create a post', async () => {
+  const client = getClient(userOne.jwt)
+  const createPost = gql`
+    mutation {
+      createPost(data: { title: "test post", body: "", published: true }) {
+        id
+        title
+        body
+        published
+      }
+    }
+  `
+  const { data } = await client.mutate({
+    mutation: createPost
+  })
+
+  expect(data.createPost.title).toBe('test post')
+  expect(data.createPost.body).toBe('')
+  expect(data.createPost.published).toBe(true)
+})
+
+test('should delete a post', async () => {
+  const client = getClient(userOne.jwt)
+  const deletePost = gql`
+    mutation {
+      deletePost(
+        id: "${postTwo.post.id}"
+      ){
+        id
+      }
+    }
+  `
+
+  await client.mutate({
+    mutation: deletePost
+  })
+
+  const exists = await prisma.exists.Post({ id: postTwo.post.id })
+
+  expect(exists).toBe(false)
 })
--- tests/utils/seedDatabase.js
+++ tests/utils/seedDatabase.js
@@ -3,50 +3,64 @@ import jwt from 'jsonwebtoken'
 import prisma from '../../src/prisma'
 
 const userOne = {
-    input: {
-        name: 'Jen',
-        email: 'jen@example.com',
-        password: bcrypt.hashSync('Red098!@#$')
-    },
-    user: undefined,
-    jwt: undefined
+  input: {
+    name: 'Jen',
+    email: 'jen@example.com',
+    password: bcrypt.hashSync('Red098!@#$')
+  },
+  user: undefined,
+  jwt: undefined
+}
+
+const postOne = {
+  input: {
+    title: 'My published post',
+    body: '',
+    published: true
+  },
+  post: undefined
+}
+
+const postTwo = {
+  input: {
+    title: 'My draft post',
+    body: '',
+    published: false
+  },
+  post: undefined
 }
 
 const seedDatabase = async () => {
-    // Delete test data
-    await prisma.mutation.deleteManyPosts()
-    await prisma.mutation.deleteManyUsers()
+  // Delete test data
+  await prisma.mutation.deleteManyPosts()
+  await prisma.mutation.deleteManyUsers()
 
-    // Create user one
-    userOne.user = await prisma.mutation.createUser({
-        data: userOne.input
-    })
-    userOne.jwt = jwt.sign({ userId: userOne.user.id }, process.env.JWT_SECRET)
+  // Create user one
+  userOne.user = await prisma.mutation.createUser({
+    data: userOne.input
+  })
+  userOne.jwt = jwt.sign({ userId: userOne.user.id }, process.env.JWT_SECRET)
 
-    await prisma.mutation.createPost({
-        data: {
-            title: 'My published post',
-            body: '',
-            published: true,
-            author: {
-                connect: {
-                    id: userOne.user.id
-                }
-            }
+  postOne.post = await prisma.mutation.createPost({
+    data: {
+      ...postOne.input,
+      author: {
+        connect: {
+          id: userOne.user.id
         }
-    })
-    await prisma.mutation.createPost({
-        data: {
-            title: 'My draft post',
-            body: '',
-            published: false,
-            author: {
-                connect: {
-                    id: userOne.user.id
-                }
-            }
+      }
+    }
+  })
+  postTwo.post = await prisma.mutation.createPost({
+    data: {
+      ...postTwo.input,
+      author: {
+        connect: {
+          id: userOne.user.id
         }
-    })
+      }
+    }
+  })
 }
 
-export { seedDatabase as default, userOne }
+export { seedDatabase as default, userOne, postOne, postTwo }
